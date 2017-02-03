645cb43a064759cfcf687c97c61096b3aed92ee9
suma-r <suma.r@onedirect.in>
2017-02-03 13:00:11 +0530

    Create SampleSauceTest.java

---
+++ SampleSauceTest.java
@@ -0,0 +1,30 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+ 
+import java.net.URL;
+ 
+public class SampleSauceTest {
+ 
+  public static final String USERNAME = "sumaR";
+  public static final String ACCESS_KEY = "8704ebb1-425f-45b4-ac9e-a624b8583f9c";
+  public static final String URL = "https://" + USERNAME + ":" + ACCESS_KEY + "@ondemand.saucelabs.com:443/wd/hub";
+ 
+  public static void main(String[] args) throws Exception {
+ 
+    DesiredCapabilities caps = DesiredCapabilities.chrome();
+    caps.setCapability("platform", "Windows XP");
+    caps.setCapability("version", "43.0");
+ 
+    WebDriver driver = new RemoteWebDriver(new URL(URL), caps);
+ 
+    /**
+     * Goes to Sauce Lab's guinea-pig page and prints title
+     */
+ 
+    driver.get("https://saucelabs.com/test/guinea-pig");
+    System.out.println("title of page is: " + driver.getTitle());
+ 
+    driver.quit();
+  }
+}
